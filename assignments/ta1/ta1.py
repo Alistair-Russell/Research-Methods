3b0371b823fc4e49b23be2aac347c12d443b769d
Alistair <alistair.russell.work@gmail.com>
2022-08-27 21:32:01 -0700

    Added authorship. Updated comments for functionality stubs as well as verbose docstrings."

--- assignments/ta1/ta1.py
+++ assignments/ta1/ta1.py
@@ -1,38 +1,39 @@
+"""Delta Hedging module
+
+This module executes delta hedging code for trading assignment 1
+"""
+
+__version__ = "0.1"
+__author__ = "Alistair Russell"
+
 from math import log, sqrt, exp
 import datetime as dt  # date objects
 import numpy as np  # array manipulation
 import matplotlib.pyplot as plot  # plotting
 import pandas as pd  # data analysis
-import pandas_datareader as pdr
-from scipy.stats import norm
+import pandas_datareader as pdr  # datareader
+from scipy.stats import norm  # normal cdf
 
 from ib_insync import *
 
 
-def d1(S, K, T, r, sigma):
-    return (log(S / K) + (r + sigma**2 / 2.0) * T) / (sigma * sqrt(T))
-
-
-def d2(S, K, T, r, sigma):
-    return d1(S, K, T, r, sigma) - sigma * sqrt(T)
-
-
-def bs_call(S, K, T, r, sigma):
-    return S * norm.cdf(d1(S, K, T, r, sigma)) - K * exp(-r * T) * norm.cdf(
-        d2(S, K, T, r, sigma)
-    )
-
-
-def bs_put(S, K, T, r, sigma):
-    return K * exp(-r * T) - S * bs_call(S, K, T, r, sigma)
-
-
 class SpxOptions:
-    def __init__(self):
-        """Initialize IB connection and create a contract for the underlying"""
+    """Class for pricing and hedging of SPX options
+
+    Returns:
+        obj: object with ib connection established and spx priced
+    """
+
+    def __init__(self, url="127.0.0.1", port=4002):
+        """Initialize IB connection and create a contract for the underlying
+
+        Args:
+            url (str, optional): URL string for TWS or IB gateway. Defaults to "127.0.0.1".
+            port (int, optional): Port number. Defaults to 4002.
+        """
         # ibkr connection
         self.ib = IB()
-        self.ib.connect("127.0.0.1", 4002, clientId=1)
+        self.ib.connect(url, port, clientId=1)
 
         # price the underlying security
         spx = Index("SPX", "CBOE")
@@ -41,8 +42,8 @@ class SpxOptions:
         [ticker] = self.ib.reqTickers(spx)
         self.spx_price = ticker.marketPrice()
 
-    def get_option_chain(self):
-        """Generate the option chain generator"""
+    def _get_option_chain(self):
+        """Create the option chain generator"""
         opt_chain = self.ib.reqSecDefOptParams(
             self.spx.symbol, "", self.spx.secType, self.spx.conId
         )
@@ -51,21 +52,54 @@ class SpxOptions:
         )
         return chain
 
-    def bs_price():
-        """Price an option with the black-scholes model"""
+    def _bs_price(S, K, T, r, sigma):
+        """Price an option with the black-scholes model
+
+        Args:
+            S (float): Price of the underlying security
+            K (float): Strike price
+            T (float): time to maturity
+            r (float): risk free interest rate
+            sigma (float): volatility of the underlying security
+
+        Returns:
+            float: the black-scholes price of the call option
+        """
+        d1 = (log(S / K) + (r + sigma**2 / 2.0) * T) / (sigma * sqrt(T))
+        d2 = (S, K, T, r, sigma) - sigma * sqrt(T)
+        bs_call = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
+        return bs_call
+
+    def _delta_hedge(self):
+        """Initiate a delta hedge for call options"""
+        # calculate the option delta, either wait for the ibkr delta or use vollib
+
+        # use the delta to determine the amount of stock needed to hedge
+
+        # calculate the difference between the hedge requirement and the current portfolio
+
+        # return trade required
         pass
 
-    def delta_hedge(self):
-        """Initiate a delta hedge for call options"""
+    def sell_mispricing(self):
+        """Sell 10 contracts of the option with the largest mispricing"""
         pass
 
     def rebalance(self):
         """Rebalance portfolio to remain delta-neutral
         Runs twice daily
         """
+        # price the option with Black-Scholes
+
+        # calculate the delta hedge trade needed to rebalance
+
+        # check whether the risk outweighs the transaction cost
+
+        # execute the trade to remain delta-neutral
         pass
 
 
 if __name__ == "__main__":
     s = SpxOptions()
+    s.rebalance()
     print(s)
