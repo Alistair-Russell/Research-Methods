1233862925757ded881f228e4eebc20c75e1c7a3
Alistair <alistair.russell.work@gmail.com>
2022-09-12 20:40:55 -0700

    Delta hedging updates for BS pricing/delta and order placement

--- assignments/ta1/ta1.py
+++ assignments/ta1/ta1.py
@@ -6,7 +6,6 @@ This module executes delta hedging code for trading assignment 1
 __version__ = "0.1"
 __author__ = "Alistair Russell"
 
-from math import log, sqrt, exp
 import datetime as dt  # date objects
 import numpy as np  # array manipulation
 import matplotlib.pyplot as plot  # plotting
@@ -28,8 +27,8 @@ class SpxOptions:
         """Initialize IB connection and create a contract for the underlying
 
         Args:
-            url (str, optional): URL string for TWS or IB gateway. Defaults to "127.0.0.1".
-            port (int, optional): Port number. Defaults to 4002.
+            url (str, optional): URL string for TWS or IB gateway. Defaults to "127.0.0.1"
+            port (int, optional): Port number for TWS or IB gateway. Defaults to 4002
         """
         # ibkr connection
         self.ib = IB()
@@ -41,18 +40,44 @@ class SpxOptions:
         self.ib.reqMarketDataType(4)
         [ticker] = self.ib.reqTickers(spx)
         self.spx_price = ticker.marketPrice()
+        return self.ib
 
-    def _get_option_chain(self):
-        """Create the option chain generator"""
+    def _get_option_contracts(self, rights=["C"]):
+        """Fetch SPX options contracts"""
         opt_chain = self.ib.reqSecDefOptParams(
             self.spx.symbol, "", self.spx.secType, self.spx.conId
         )
         chain = next(
             c for c in opt_chain if c.tradingClass == "SPX" and c.exchange == "SMART"
         )
-        return chain
+        strikes = [
+            strike
+            for strike in chain.strikes
+            if strike % 5 == 0 and spxValue - 20 < strike < spxValue + 20
+        ]
+        expirations = sorted(exp for exp in chain.expirations)[:2]
 
-    def _bs_price(S, K, T, r, sigma):
+        contracts = [
+            Option("SPX", expiration, strike, right, "SMART", tradingClass="SPX")
+            for right in rights
+            for expiration in expirations
+            for strike in strikes
+        ]
+        cons = self.ib.qualifyContracts(*contracts)
+        return cons
+
+    def sell_mispricing(self, num_contracts):
+        """Sell contracts of the option with the largest mispricing
+
+        Args:
+            num_contracts (int): the number of contracts to sell
+        """
+        # get option chain
+        # contracts = [c for c in self._get_option_contracts]
+        # order = MarketOrder("SELL", num_contracts)
+        pass
+
+    def _bs_price(S, K, T, r, sigma, opt="c"):
         """Price an option with the black-scholes model
 
         Args:
@@ -61,33 +86,60 @@ class SpxOptions:
             T (float): time to maturity
             r (float): risk free interest rate
             sigma (float): volatility of the underlying security
+            opt (str, optional): option type. Defaults to "C" (call)
 
         Returns:
-            float: the black-scholes price of the call option
+            float: the black-scholes price of the option
         """
-        d1 = (log(S / K) + (r + sigma**2 / 2.0) * T) / (sigma * sqrt(T))
-        d2 = (S, K, T, r, sigma) - sigma * sqrt(T)
-        bs_call = S * norm.cdf(d1) - K * exp(-r * T) * norm.cdf(d2)
-        return bs_call
+        d1 = (np.log(S / K) + (r + sigma**2 / 2.0) * T) / (sigma * np.sqrt(T))
+        d2 = d1 - sigma * np.sqrt(T)
+
+        if opt == "c":
+            bs_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+        elif opt == "p":
+            bs_price = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+        else:
+            bs_price = None
+        return bs_price
+
+    def _bs_delta(S, K, T, r, sigma, opt="C"):
+        """Calculate the black-scholes delta of the option
+
+        Args:
+            S (float): Price of the underlying security
+            K (float): Strike price
+            T (float): time to maturity
+            r (float): risk free interest rate
+            sigma (float): volatility of the underlying security
+            opt (str, optional): option type. Defaults to "C" (call)
+
+        Returns:
+            float: the black-scholes delta of the option
+        """
+        d1 = (np.log(S / K) + (r + sigma**2 / 2.0) * T) / (sigma * np.sqrt(T))
+
+        if opt == "C":
+            bs_delta = norm.cdf(d1)
+        elif opt == "P":
+            bs_delta = -norm.cdf(-d1)
+        else:
+            bs_delta = None
+        return bs_delta
 
     def _delta_hedge(self):
         """Initiate a delta hedge for call options"""
-        # calculate the option delta, either wait for the ibkr delta or use vollib
-
         # use the delta to determine the amount of stock needed to hedge
+        price = self._bs_delta()
+        delta = self._bs_delta()
+        hedge = delta * 10
 
         # calculate the difference between the hedge requirement and the current portfolio
+        positions = [p for p in self.ib.positions()]
 
-        # return trade required
-        pass
-
-    def sell_mispricing(self, num_contracts):
-        """Sell contracts of the option with the largest mispricing
-
-        Args:
-            num_contracts (int): the number of contracts to sell
-        """
-        pass
+        # return contract and order required to remain delta neutral
+        contract = Index("SPX", "CBOE")
+        order = MarketOrder("BUY", hedge)
+        return (contract, order)
 
     def rebalance(self):
         """Rebalance portfolio to remain delta-neutral
@@ -96,14 +148,16 @@ class SpxOptions:
         # price the option with Black-Scholes
 
         # calculate the delta hedge trade needed to rebalance
+        contract, order = self._delta_hedge()
 
         # check whether the risk outweighs the transaction cost
+        # ib.whatIfOrder(contract, order)
+        dryrun_order = self.ib.whatIfOrder(contract, order)
+        # if dryrun_order.commission >
 
         # execute the trade to remain delta-neutral
-        pass
-
-
-if __name__ == "__main__":
-    s = SpxOptions()
-    s.rebalance()
-    print(s)
+        trade = self.ib.placeOrder(contract, order)
+        assert order in self.ib.orders()
+        assert trade in self.ib.trades()
+        while not trade.isDone():
+            self.ib.waitOnUpdate()
