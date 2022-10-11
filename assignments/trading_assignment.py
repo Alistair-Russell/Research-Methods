630f1e403fdd024d78a1387a0f1504dc7d27003a
Alistair <alistair.russell.work@gmail.com>
2022-10-11 09:46:35 -0700

    Working trading assignments consolidated. Added execution in jupyter notebook

--- assignments/trading_assignment.py
+++ assignments/trading_assignment.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 import itertools as it
 from ib_insync import *
 
@@ -120,7 +121,8 @@ class DeltaHedgeAlgo(BaseAlgo):
 
         # get option delta from modelGreeks to calculate the delta neutral position
         delta = opt_ticker.modelGreeks.delta
-        delta_neutral_pos = -round(option.position * delta * int(opt_ticker.multiplier))
+        multiplier = int(opt_ticker.contract.multiplier)
+        delta_neutral_pos = -round(option.position * delta * multiplier)
 
         # determine the trade needed to reach the delta neutral position
         if len(self.stks) == 0:
@@ -148,13 +150,13 @@ class DeltaHedgeAlgo(BaseAlgo):
 class VIXFuturesHedgeAlgo(BaseAlgo):
     """(TA2) Class for hedging of VIX futures with E-mini-S&P futures"""
 
-    def __init__(self, vix_future_date, hedge_tic, **kwargs):
+    def __init__(self, vix_future_date, es_future_date, **kwargs):
         super(VIXFuturesHedgeAlgo, self).__init__(**kwargs)
 
         # initialize vix spot and futures dictionaries
-        self.ticker = hedge_tic
         self.vix_spot = {}
         self.vix_future = {}
+        self.es_future = {}
 
         # request delayed market data
         self.ibconn.reqMarketDataType(4)
@@ -167,13 +169,21 @@ class VIXFuturesHedgeAlgo(BaseAlgo):
         [future_tic] = self.ibconn.reqTickers(future)
         self.vix_future["ticker"] = future_tic
 
-        # construct backwardation signal
+        # vix spot contract for date provided
         index = Index("VIX")
         self.ibconn.qualifyContracts(index)
         [spot_con] = self.ibconn.reqContractDetails(index)
         self.vix_spot["contract"] = spot_con
         [spot_tic] = self.ibconn.reqTickers(index)
         self.vix_spot["ticker"] = spot_tic
+
+        # es future contract for date provided
+        es = Future("ES", es_future_date, "GLOBEX")
+        self.ibconn.qualifyContracts(es)
+        [es_con] = self.ibconn.reqContractDetails(es)
+        self.es_future["contract"] = es_con
+        [es_tic] = self.ibconn.reqTickers(es)
+        self.es_future["ticker"] = es_tic
 
     def _get_es_beta(self):
         ## Q4- E-mini and VIX Futures Historical Data
@@ -216,25 +226,24 @@ class VIXFuturesHedgeAlgo(BaseAlgo):
         beta = model.params.values[1]
         return beta
 
-    def hedge(self, future_es_date, dryrun=False):
-        # get es pricing
-        self.ibconn.reqMarketDataType(4)
-        future = Future("ES", future_es_date, "GLOBEX")
-        self.ibconn.qualifyContracts(future)
-        [es_con] = self.ibconn.reqContractDetails(future)
-        [es_tic] = self.ibconn.reqTickers(future)
-        es_price = es_tic.last
-
+    def hedge(self, quantity, dryrun=False):
         # calculate hedge amount
         beta = self._get_es_beta()
-        hedge_amt = round(
-            (beta * self.vix_future["ticker"].last * 1000)
-            / (es_price * self.vix_future["contract"].multiplier)
+        hedge_amt = (
+            -1
+            * quantity
+            * round(
+                (beta * float(self.vix_future["ticker"].last) * 1000)
+                / (float(self.es_future["ticker"].last) * 50)
+            )
         )
 
         # place hedge trade
+        es_con = self.es_future["contract"].contract
         if abs(hedge_amt) > 0:
+            print(f"Attempting to hedge with E-mini: amount {hedge_amt}")
             trade = self.market_order(es_con, hedge_amt, dryrun=dryrun)
+
             if not dryrun:
                 while not trade.isDone():
                     self.ibconn.waitOnUpdate()
@@ -243,31 +252,85 @@ class VIXFuturesHedgeAlgo(BaseAlgo):
         else:
             print("Already hedged, no trade required.")
 
+    def enter_positions(self, quantity):
+        # if there are open vix positions then return, otherwise attempt to enter
+        vix_positions = [
+            p for p in self.ibconn.positions() if p.contract.symbol == "VIX"
+        ]
+        if len(vix_positions) > 0:
+            print(f"VIX positions already entered: {vix_positions}")
+            return
+
+        # get contract
+        vix_con = self.vix_future["contract"].contract
+        vixf_price = self.vix_future["ticker"].last
+        vixs_price = self.vix_spot["ticker"].last
+
+        # calculate the contango/backwardation signal
+        signal = (vixf_price / vixs_price) - 1
+
+        # calculate the daily roll
+        future_date = vix_con.lastTradeDateOrContractMonth
+        days = bus_day_delta(future_date)
+        daily_roll = (vixf_price - vixs_price) / days
+
+        # initial trade
+        if signal < 0 and daily_roll < -0.10:
+            print(
+                "Market backwardation - daily roll is less than -0.10. Purchasing VIX Futures."
+            )
+            trade = self.market_order(vix_con, quantity)
+            self.hedge(self, -1 * quantity)
+        elif signal > 0 and daily_roll > 0.10:
+            print(
+                "Market contango - daily roll is more than 0.10. Shorting VIX Futures."
+            )
+            trade = self.market_order(vix_con, -1 * quantity)
+            self.hedge(self, quantity)
+        else:
+            print(
+                f"Signal is {signal} and daily roll is {daily_roll}. No action taken."
+            )
+
     def exit_positions(self):
-        vix_pos = [
+        f = [
             p
             for p in self.ibconn.positions()
             if p.contract.secType == "FUT" and p.contract.symbol == "VIX"
         ]
-
-        for f in vix_pos:
-            d = dt.datetime.strptime(
-                f.contract.lastTradeDateOrContractMonth, "%Y%m%d"
-            ).date()
-            days = bus_day_delta(d)
-            daily_roll = (
-                self.vix_future["ticker"].last - self.vix_spot["ticker"].last
-            ) / days  # TODO make a daily roll fn
-
-            is_contango_takeprofit = f.position < 0 and (days <= 9 or daily_roll < 0.05)
-            is_backwardation_takeprofit = f.position > 0 and (
-                days <= 9 or daily_roll > -0.05
+        e = [
+            p
+            for p in self.ibconn.positions()
+            if p.contract.secType == "FUT" and p.contract.symbol == "ES"
+        ]
+        if len(f) == 0 and len(e) == 0:
+            print("No positions to exit.")
+            return
+        elif len(f) == 0 or len(e) == 0:
+            print(f"There are unhedged positions: {f} {e}.")
+            print(
+                "Please hedge manually or use VIXFuturesHedgeAlgo.hedge(position) and retry"
             )
+            return
 
-            # in either takeprofit scenario, liquidate the positions
-            if is_contango_takeprofit or is_backwardation_takeprofit:
-                trade = self.market_order(f.contract, -1 * f.position)
-                # TODO: liquidate the es position
+        # take the first positions
+        vixf = f[0]
+        esf = e[0]
+
+        d = vixf.contract.lastTradeDateOrContractMonth
+        days = bus_day_delta(d)
+        daily_roll = (
+            self.vix_future["ticker"].last - self.vix_spot["ticker"].last
+        ) / days  # TODO make a daily roll fn
+
+        is_contango_takeprofit = vixf.position < 0 and (days <= 9 or daily_roll < 0.05)
+        is_backwd_takeprofit = vixf.position > 0 and (days <= 9 or daily_roll > -0.05)
+
+        # in either takeprofit scenario, liquidate the positions
+        if is_contango_takeprofit or is_backwd_takeprofit:
+            print("Exiting VIX and ES positions")
+            vixtrade = self.market_order(vixf.contract, -1 * vixf.position)
+            estrade = self.market_order(esf.contract, -1 * esf.position)
 
 
 class PairsTradingAlgo(BaseAlgo):
@@ -353,9 +416,7 @@ class PairsTradingAlgo(BaseAlgo):
 
     def rebalance(self):
         # exit any open pairs traded positions
-        positions = self.positions = [
-            p for p in self.ibconn.positions() if p.contract.secType == "STK"
-        ]
+        positions = [p for p in self.ibconn.positions() if p.contract.secType == "STK"]
 
         # set a max portfolio allotment based on the number of positions
         max_allotment = min(
